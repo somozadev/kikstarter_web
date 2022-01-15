b2f200af2c839cf659dcd4de563fbcfe1be0cab2
somozadev <somozadev@gmail.com>
2022-01-15 14:55:36 +0100

    main page bonica, falta hovers que no he sido capaz

--- proyect/index.php
+++ proyect/index.php
@@ -7,11 +7,27 @@
 
   <div class="row row-cols-1 row-cols-lg-3 align-items-stretch g-4 py-5">
     <div class="col">
-      <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-10 shadow-lg" style="background-repeat:no-repeat;
-  background-position: center; background-image: url('images/car.png'); 	border-radius: 50px; border-color: #7a83c8; border-style: solid;">
-        <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-          <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold" style="text-align: center;  color:#b5bcec;">Próxima generación de coches</h2>
-          <!-- <ul class="d-flex list-unstyled mt-auto ">
+        <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-10 shadow-lg" style="background-repeat:no-repeat;
+  background-position: center; background-image: url('images/car.png'); background-size: cover;
+	border-width: 10px;	border-radius: 50px; border-color: #7a83c8; border-style: solid;">
+          <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+            <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold" style="text-align: center;  color:#b5bcec;">Próxima generación de coches</h2>
+            <!-- <ul class="d-flex list-unstyled mt-auto ">
+            <li class="d-flex align-items-center me-3">
+              <small>...David Sanz</small>
+            </li>
+          </ul> -->
+          </div>
+      </div>
+    </div>
+
+    <div class="col">
+        <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-10 shadow-lg" style="background-repeat:no-repeat;
+  background-position: center;background-image: url('images/motorbike.png');  background-size: cover;
+	border-width: 10px;	border-radius: 50px; border-color: #7a83c8; border-style: solid;">
+          <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
+            <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold " style="text-align: center;  color:#b5bcec;">Motos vanguardistas</h2>
+            <!-- <ul class="d-flex list-unstyled mt-auto ">
             <li class="d-flex align-items-center me-3">
               <small>...David Sanz</small>
             </li>
@@ -21,35 +37,22 @@
     </div>
 
     <div class="col">
-      <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-10 shadow-lg" style="background-repeat:no-repeat;
-  background-position: center;background-image: url('images/motorbike.png');  	border-radius: 50px; border-color: #7a83c8; border-style: solid;">
-        <div class="d-flex flex-column h-100 p-5 pb-3 text-white text-shadow-1">
-          <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold " style="text-align: center;  color:#b5bcec;">Motos vanguardistas</h2>
-          <!-- <ul class="d-flex list-unstyled mt-auto ">
+        <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-repeat:no-repeat;
+  background-position: center;background-image: url('images/dron.png'); background-size: cover;
+	border-width: 10px; 	border-radius: 50px; border-color: #7a83c8; border-style: solid;">
+          <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
+            <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold" style="text-align: center;  color:#b5bcec;">Drones, transporte ágil y rápido</h2>
+            <!-- <ul class="d-flex list-unstyled mt-auto ">
             <li class="d-flex align-items-center me-3">
               <small>...David Sanz</small>
             </li>
           </ul> -->
+          </div>
         </div>
-      </div>
-    </div>
-
-    <div class="col">
-      <div class="card card-cover h-100 overflow-hidden text-white bg-dark rounded-5 shadow-lg" style="background-repeat:no-repeat;
-  background-position: center;background-image: url('images/dron.png');  	border-radius: 50px; border-color: #7a83c8; border-style: solid;">
-        <div class="d-flex flex-column h-100 p-5 pb-3 text-shadow-1">
-          <h2 class="pt-5 mt-5 mb-4 display-6 lh-1 fw-bold" style="text-align: center;  color:#b5bcec;">Drones, transporte ágil y rápido</h2>
-          <!-- <ul class="d-flex list-unstyled mt-auto ">
-            <li class="d-flex align-items-center me-3">
-              <small>...David Sanz</small>
-            </li>
-          </ul> -->
-        </div>
-      </div>
     </div>
   </div>
 </div>
-<div class="row">
+<!-- <div class="row">
   <div class="column">
     <a href="carousel.php"> <img src="images/coche.png">
       <div class="infoImg">
@@ -69,5 +72,5 @@
       <p><b>Drones</b>, transporte ágil y rápido</b></p>
     </div>
   </div>
-</div>
+</div> -->
 <?php include_once('footer.php') ?>
